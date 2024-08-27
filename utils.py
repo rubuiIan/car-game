9d890c49f613b6c3c883451207b038c2932f8458
ANGELA KARIUKI <iyanrubui@gmail.com>
2024-08-27 22:42:49 +0300

    moving the car

--- utils.py
+++ utils.py
@@ -1,13 +1,10 @@
 import pygame
 
-
 def scale_image(img, factor):
     size = round(img.get_width() * factor), round(img.get_height() * factor)
     return pygame.transform.scale(img, size)
 
-
-def blit_rotate_center(win, image, top_left, angle):
+def blit_rotate_center(win, image, topleft, angle):
     rotated_image = pygame.transform.rotate(image, angle)
-    new_rect = rotated_image.get_rect(
-        center=image.get_rect(top_left = top_left).center)
-    win.blit = (rotated_image, new_rect.topleft)
+    new_rect = rotated_image.get_rect(center=image.get_rect(topleft=topleft).center)
+    win.blit(rotated_image, new_rect.topleft)
