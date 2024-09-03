898c758980fa2c67e7719fad44f197463daf0e8c
ANGELA KARIUKI <iyanrubui@gmail.com>
2024-09-03 13:58:20 +0300

    final commit

--- utils.py
+++ utils.py
@@ -8,3 +8,8 @@ def blit_rotate_center(win, image, topleft, angle):
     rotated_image = pygame.transform.rotate(image, angle)
     new_rect = rotated_image.get_rect(center=image.get_rect(topleft=topleft).center)
     win.blit(rotated_image, new_rect.topleft)
+    
+def blit_text_center(win, font, text):
+    render = font.render(text, 1, (200, 200, 200))
+    win.blit(render, (win.get_width()/2 - render.get_width() / 2,
+                      win.get_height()/2 - win.get_height()/2))
